596d9bdca84fd7a658a445ae51f5ae406497254a
Kavi-Dew-23 <dewapuraka@gmail.com>
2026-03-29 20:47:17 +0530

    Configure 8080 port for deployment

--- FlowDesk.API/Program.cs
+++ FlowDesk.API/Program.cs
@@ -78,4 +78,6 @@ app.MapControllers();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.Run();
+//app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://0.0.0.0:{port}");
