094f443b0d32b702b83253374ded09e34fd2d313
Kavi-Dew-23 <dewapuraka@gmail.com>
2026-03-29 15:35:47 +0530

    Add unit tests

--- FlowDesk.Tests/UnitTest1.cs
+++ FlowDesk.Tests/UnitTest1.cs
@@ -1,10 +1,73 @@
-﻿namespace FlowDesk.Tests;
+﻿using FlowDesk.Domain.Entities;
+using FlowDesk.Domain.Enums;
+using FluentAssertions;
 
-public class UnitTest1
+namespace FlowDesk.Tests;
+
+public class TaskItemTransitionTests
 {
     [Fact]
-    public void Test1()
+    public void ToDo_Can_Transition_To_InProgress()
     {
+        var task = new TaskItem { Status = BoardTaskStatus.ToDo };
+        task.TransitionTo(BoardTaskStatus.InProgress);
+        task.Status.Should().Be(BoardTaskStatus.InProgress);
+    }
 
+    [Fact]
+    public void ToDo_Cannot_Transition_To_Done()
+    {
+        var task = new TaskItem { Status = BoardTaskStatus.ToDo };
+        var act = () => task.TransitionTo(BoardTaskStatus.Done);
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void ToDo_Cannot_Transition_To_Archived()
+    {
+        var task = new TaskItem { Status = BoardTaskStatus.ToDo };
+        var act = () => task.TransitionTo(BoardTaskStatus.Archived);
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void InProgress_Can_Transition_To_Done()
+    {
+        var task = new TaskItem { Status = BoardTaskStatus.InProgress };
+        task.TransitionTo(BoardTaskStatus.Done);
+        task.Status.Should().Be(BoardTaskStatus.Done);
+    }
+
+    [Fact]
+    public void InProgress_Can_Go_Back_To_ToDo()
+    {
+        var task = new TaskItem { Status = BoardTaskStatus.InProgress };
+        task.TransitionTo(BoardTaskStatus.ToDo);
+        task.Status.Should().Be(BoardTaskStatus.ToDo);
+    }
+
+    [Fact]
+    public void Done_Can_Transition_To_Archived()
+    {
+        var task = new TaskItem { Status = BoardTaskStatus.Done };
+        task.TransitionTo(BoardTaskStatus.Archived);
+        task.Status.Should().Be(BoardTaskStatus.Archived);
+    }
+
+    [Fact]
+    public void Archived_Cannot_Transition_To_Anything()
+    {
+        var task = new TaskItem { Status = BoardTaskStatus.Archived };
+        var act = () => task.TransitionTo(BoardTaskStatus.ToDo);
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void Transition_Updates_UpdatedAt_Timestamp()
+    {
+        var task = new TaskItem { Status = BoardTaskStatus.ToDo };
+        var before = task.UpdatedAt;
+        task.TransitionTo(BoardTaskStatus.InProgress);
+        task.UpdatedAt.Should().BeOnOrAfter(before);
     }
 }
